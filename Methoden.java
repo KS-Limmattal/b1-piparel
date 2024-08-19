bf59e703f6845489ffa22a749b62e839f4edac06
piparel <valdmanis299@gmail.com>
2024-08-19 15:50:17 +0200

    Aufgabe a gelöst

--- Methoden.java
+++ Methoden.java
@@ -1,12 +1,22 @@
 public class Methoden {
 
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
+
+    public static String hours(int seconds){
+        int hours = seconds / 3600;
+        seconds -= hours * 3600;
+        int minutes = seconds / 60;
+        seconds -= minutes * 60;
+        return hours + ":" + minutes + ":" + seconds;
+    }
     
     public static void main(String[] args){
         // Test-Code für Teilaufgabe a)
         System.out.println("Aufgabe a)");
         System.out.println(hours(3));
         System.out.println(hours(75));
+        System.out.println(hours(6593));
+
         // TODO: Schreibe mehr Testcode
         System.out.println();
 
