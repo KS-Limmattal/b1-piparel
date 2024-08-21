4b9aee6f23b564cea2d8e3da0df2a6df2efcc022
piparel <valdmanis299@gmail.com>
2024-08-21 08:37:56 +0200

    arbeiten bei aufgabe d

--- Loops.java
+++ Loops.java
@@ -1,21 +1,33 @@
 public class Loops {
     
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
+    public static String primeFactorisation(int n){
+        String answer = "";
+        for(int i = 2; i<=n;i++){
+            if (n % i == 0){
+                answer = answer + i + "*";
+                n = n/i;
+                i = 2;
+            }
+
+        }
+        return "Prime factorisation of" + answer + "1";
+    }
     
     public static void main(String[] args){
         
         // Test-Code für Teilaufgabe c)
         System.out.println("Aufgabe c)");
-        System.out.println(hours(3));
-        System.out.println(hours(75));
+        System.out.println(primeFactorisation(12));
+        System.out.println(primeFactorisation(82));
         // TODO: Schreibe mehr Testcode
         System.out.println();
 
         // Test-Code für Teilaufgabe d)
         System.out.println("Aufgabe d)");
-        for (int n = 0; n < 12; n++){
-            System.out.println("Pi approximated to " + n + " digits with pi()  is " + pi(n));
+        //for (int n = 0; n < 12; n++){
+            //System.out.println("Pi approximated to " + n + " digits with pi()  is " + pi(n));
         }
-        System.out.println();
+        //System.out.println();
     }
-}
+//}
