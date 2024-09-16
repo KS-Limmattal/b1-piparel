918f80e522b192ba75d5a6ab5eb1b025656da6e9
piparel <valdmanis299@gmail.com>
2024-09-16 12:47:40 +0200

    finished

--- Arrayz.java
+++ Arrayz.java
@@ -1,20 +1,19 @@
+import java.util.Arrays;
+ 
 public class Arrayz {
-    
-    // TODO: Schreibe hier die Methoden für die Teilaufgaben.
-    
-
+ 
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
     public static double[] sort(double[] input_list) {
         boolean swapped;
-        for (int j = 0; j < input_list.length - 1; j++) {
+        for (int k = 0; k < input_list.length - 1; k++) {
             swapped = false;
  
-            for (int i = 0; i < input_list.length - 1; i++) {
+            for (int j = 0; j < input_list.length - 1; j++) {
  
-                if (input_list[i] > input_list[i + 1]) {
-                    double temp = input_list[i];
-                    input_list[i] = input_list[i + 1];
-                    input_list[i + 1] = temp;
+                if (input_list[j] > input_list[j + 1]) {
+                    double temp = input_list[j];
+                    input_list[j] = input_list[j + 1];
+                    input_list[j + 1] = temp;
                     swapped = true;
                 }
             }
@@ -24,52 +23,75 @@ public class Arrayz {
         }
         return input_list;
     }
+ 
     public static boolean isMagicSquare(int[][] square) {
-        boolean magicSquare = false; 
-        int square_end = square.length;
-        int line_sum = 0;
-        int col_sum = 0;
-        int dia_sum = 0;
-
-        for (int i = 0; i < square_end; i++) {
-            line_sum = line_sum + square[i][0];
-            for (int k = 0; k < square_end - 1; k ++){
-                col_sum = col_sum + square[0][k];
-                dia_sum = dia_sum + square[i][k];
+        int square_length = square.length;
+        int new_sum = 0;
+        int sum = 0;
+        for (int i = 0; i < square_length; i++) {
+            new_sum = new_sum + square[0][i];
+        }
+ 
+        for (int k = 0; k < square_length; k++) {
+            sum = 0;
+            for (int j = 0; j < square_length; j++) {
+                sum = sum + square[k][j];
+            }
+            System.out.println(sum);
+            if (sum != new_sum) {
+               return false; 
             }
         }
-        System.out.println(line_sum);
-        System.out.println(col_sum);
-        System.out.println(dia_sum);
-
-        if (line_sum + col_sum + dia_sum == 0) {
-            return true;
+        for (int k = 0; k < square_length; k++) {
+            sum = 0;
+            for (int j = 0; j < square_length; j++) {
+                sum = sum + square[j][k];
+            }
+            System.out.println(sum);
+            if (sum != new_sum) {
+               return false; 
+            }
         }
-        else {
-            return false;
+ 
+        sum = 0;
+        for (int l = 0; l < square_length; l ++) {
+            sum = sum + square[l][l];
         }
-        
+        if (sum != new_sum) {
+            return false; 
         }
-           
-
-    public static void main(String[] args){
-        
+ 
+        for (int m = 1; m < square_length; m++) {
+            sum = sum + square[square_length-m][square_length-m];
+        }
+        if (sum != new_sum) {
+            return false; 
+        }
+ 
+        return true;
+}
+ 
+    public static void main(String[] args) {
+ 
         // Test-Code für Teilaufgabe e)
         System.out.println("Aufgabe e)");
-        double[] nr = {123, 356.131231, 1594};
-        System.out.println(sort(nr));
-        // TODO: Schreibe mehr Testcode
+        double[] numbers = { 1, 2.3, 4 };
+        System.out.println(Arrays.toString(sort(numbers)));
+        // TODO: Schreibe mehr Testcode nein
         System.out.println();
-
+ 
         // Test-Code für Teilaufgabe f)
         System.out.println("Aufgabe f)");
-        int[][] magicSquare = new int[][] { { 12, 6, 15, 1 },
+        int[][] magicSquare = new int[][] {
+                { 12, 6, 15, 1 },
                 { 13, 3, 10, 8 },
                 { 2, 16, 5, 11 },
                 { 7, 9, 4, 14 }
         };
         System.out.println(isMagicSquare(magicSquare));
-        int[][] nonMagicSquare = new int[][] { { 1, 6, 15, 1 },
+ 
+        int[][] nonMagicSquare = new int[][] {
+                { 1, 6, 15, 1 },
                 { 13, 3, 10, 8 },
                 { 2, 16, 5, 11 },
                 { 7, 9, 4, 14 }
